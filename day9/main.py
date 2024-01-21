38cce14b4ba27aef22c8664ab7df35fc6c7c38bb
RiteshChepuri <riteshchepuri0@gmail.com>
2024-01-22 00:24:45 +0530

    feat: completed day9

---
+++ day9/main.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+with open("day9.in") as file:
+    data = file.read().strip()
+
+# print(data)
+L = data.split("\n")
+
+
+def f(xs, part2):
+    if all(x == 0 for x in xs):
+        return 0
+    D = []
+    for i in range(len(xs) - 1):
+        D.append(xs[i + 1] - xs[i])
+    return xs[0 if part2 else -1] + (-1 if part2 else 1) * f(D, part2)
+
+
+for part2 in [False, True]:
+    ans = 0
+    for line in L:
+        xs = [int(x) for x in line.split()]
+        ans += f(xs, part2)
+    print(ans)
